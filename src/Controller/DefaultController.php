7bead258bdd67530108b15ec325d26313a9b7958
Nicolas Vauche <hello@nicolasvauche.net>
2022-06-07 08:50:03 +0000

    Homepage with posts in Grid

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    private $manager;
+
+    /**
+     * @param $manager
+     */
+    public function __construct(ManagerRegistry $managerRegistry)
+    {
+        $this->manager = $managerRegistry;
+    }
+
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        // Call model, get data from DB
-
-
         return $this->render('default/index.html.twig', [
-            'controller_name' => 'Toto',
+            'posts' => $this->manager->getRepository(Post::class)->findBy([], ['title' => 'DESC']),
         ]);
     }
 }
